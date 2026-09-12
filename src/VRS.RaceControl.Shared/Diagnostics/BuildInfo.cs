89b5170491fb6f4a0d1ac7327044f6bb53784b0f
michiczajko-ctrl <michi.czajko@wp.pl>
2026-09-12 19:58:17 +0200

    Align relay deployment with Beta 5.2.F

--- src/VRS.RaceControl.Shared/Diagnostics/BuildInfo.cs
+++ src/VRS.RaceControl.Shared/Diagnostics/BuildInfo.cs
@@ -4,11 +4,11 @@ namespace VRS.RaceControl.Shared.Diagnostics;
 
 public static class BuildInfo
 {
-    public const string DisplayVersion = "Beta 5.1.F";
+    public const string DisplayVersion = "Beta 5.2.F";
 
     public static string InformationalVersion =>
         typeof(BuildInfo).Assembly
             .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
             ?.InformationalVersion
-        ?? "5.1.0-beta.F";
+        ?? "5.2.0-beta.F";
 }
